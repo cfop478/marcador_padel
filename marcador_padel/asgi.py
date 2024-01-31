68de64738ef8d81f9a596ac1d4cc4dfb8549a6ef
cfop478 <rogercampsviader@gmail.com>
2024-01-31 19:36:14 +0100

    bàsic config completed

---
+++ marcador_padel/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for marcador_padel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marcador_padel.settings')
+
+application = get_asgi_application()
